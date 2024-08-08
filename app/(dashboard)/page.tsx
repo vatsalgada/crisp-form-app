b16764d21ae7815c547456a52374763901b4a465
vatsalgada <contactvgada@gmail.com>
2024-08-09 03:09:43 +0530

    +form card complete

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,13 +4,18 @@ import { GetForms, GetFormStats } from "../lib/actions/form";
 import { LuView } from "react-icons/lu";
 import { FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
+import { BiRightArrowAlt } from "react-icons/bi";
 import { TbArrowBounce } from "react-icons/tb";
+import { FaEdit } from "react-icons/fa";
 import { ReactNode, Suspense } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Separator } from "@/components/ui/separator";
 import CreateFormBtn from "@/components/CreateFormBtn";
 import { Form } from "@prisma/client";
+import { Badge } from "@/components/ui/badge";
+import { formatDistance } from "date-fns";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -135,13 +140,43 @@ function FormCard({form}: {form: Form}) {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>
-          <span>
-           
+        <CardTitle className="flex items-center gap-2 justify-between">
+          <span className="truncate font-bold">
             {form.name}
-          </span>
+          </span> 
+          {form.published && <Badge> Published </Badge>}
+          {!form.published && <Badge> Draft </Badge>}
         </CardTitle>
+        <CardDescription className="flex items-center justify-between text-sm">
+          {formatDistance(form.createdAt, new Date(), 
+          {addSuffix: true})}
+          {
+            form.published && <span className="flex items-center gap-2"> 
+              <LuView className="" />
+              <span>{form.visits.toLocaleString()}</span>
+              <FaWpforms className="" />
+              <span>{form.submissions.toLocaleString()}</span>
+            </span>
+          }
+        </CardDescription>
       </CardHeader>
+      <CardContent className="h-[20px] truncate text-sm">
+          {form.description || "No description"}
+      </CardContent>
+      <CardFooter> 
+        {form.published && (
+          <Button asChild className="w-full mt-2 text-md gap-4"> 
+            <Link href={`/forms/${form.id}`}>View submissions <BiRightArrowAlt /></Link>
+          </Button>
+        ) }
+        {!form.published && (
+          <Button asChild 
+          variant={"secondary"}
+          className="w-full mt-2 text-md gap-4"> 
+            <Link href={`/builder/${form.id}`}>form<FaEdit /></Link>
+          </Button>
+        ) }
+      </CardFooter>
     </Card>
   )
 }
--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -13,13 +13,13 @@ import { Textarea } from './ui/textarea';
 import { toast } from './ui/use-toast';
 import { CreateForm } from '@/app/lib/actions/form';
 import { BsFileEarmarkPlus } from "react-icons/bs";
-
-
+import { useRouter } from 'next/navigation';
 
 
 
 export default function CreateFormBtn() {
 
+    const router = useRouter();
     const form = useForm<formSchemaType>({
         resolver: zodResolver(formSchema),
     });
@@ -31,7 +31,8 @@ export default function CreateFormBtn() {
                 title: "Success",
                 description: "Form created successfully"
             })
-            console.log(formId)
+            router.push(`/builder/${formId}`)
+         
             }
          catch (error) {
             toast({
